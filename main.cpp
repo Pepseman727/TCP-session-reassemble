b935a337275e082dec8943ba8beec8c421f6ccb0
Kirill <pepseman727@gmail.com>
2020-11-27 02:48:22 +0300

    Adding Cmake

--- main.cpp
+++ main.cpp
@@ -5,15 +5,15 @@
 #include <malloc.h>
 
 #include <pcap.h>
-/*
-#include <netinet/in.h>
+
+/*#include <netinet/in.h>
 #include <netinet/ip.h>
 #include <net/if.h>
 #include <netinet/if_ether.h>
 #include <net/ethernet.h>
 #include <netinet/tcp.h>
-#include <arpa/inet.h>*/
-
+#include <arpa/inet.h>
+*/
 #include <inttypes.h>
 
 #define PCAP_BUF_SIZE 2048
